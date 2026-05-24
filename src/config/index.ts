26abb9a595e62877f717171a7b7a7af9600ea9e7
Somil Gupta <gsomil93@gmail.com>
2026-05-24 11:56:12 +0530

    finalizing starter evaluation

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 
 type ServerConfig = {
     PORT: number,
-    DB_URL: string,
-    PROBLEM_SERVICE_URL: string,
+    // DB_URL: string,
+    SUBMISSION_SERVICE_URL: string,
 }
 
 function loadEnv() {
@@ -15,7 +15,7 @@ function loadEnv() {
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3002,
-    DB_URL: process.env.DB_URL as string || "mongodb://localhost:27017/evaluation-service",
-    PROBLEM_SERVICE_URL: process.env.PROBLEM_SERVICE_URL as string || "http://localhost:3000/api/v1",
+    PORT: Number(process.env.PORT) || 3003,
+    // DB_URL: process.env.DB_URL as string || "mongodb://localhost:27017/evaluation-service",
+    SUBMISSION_SERVICE_URL: process.env.SUBMISSION_SERVICE_URL as string || "http://localhost:3000/api/v1",
 };
